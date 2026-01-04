e7e6dba76faf8afc35371c2155b892044f7bf778
dawoodriaza <od.riaz@alsalambank.com>
2026-01-04 18:16:56 +0300

    Added a java Src file

---
+++ src/main/java/com/ga/airticketmanagement/AirticketmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.ga.airticketmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirticketmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirticketmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ga/airticketmanagement/AirticketmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ga.airticketmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirticketmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
