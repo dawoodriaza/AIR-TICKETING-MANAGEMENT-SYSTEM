865d27d559170dc9092e090bec007cbf112164d0
Husain Habib <habib.h.e@outlook.com>
2026-01-07 00:56:12 +0300

    remove unintentional auto imports

--- src/main/java/com/ga/airticketmanagement/service/AirportService.java
+++ src/main/java/com/ga/airticketmanagement/service/AirportService.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
