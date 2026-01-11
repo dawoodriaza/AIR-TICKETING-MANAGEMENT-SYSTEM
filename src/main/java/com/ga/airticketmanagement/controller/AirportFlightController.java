ef449be9207ce91e8a2f00901bfd1a5687cbb5bc
Husain Habib <habib.h.e@outlook.com>
2026-01-11 16:32:11 +0300

    update return types to FlightResponse, pre-authorize CUD endpoints for ADMIN user role, change http status to 204 for delete endpoint

--- src/main/java/com/ga/airticketmanagement/controller/AirportFlightController.java
+++ src/main/java/com/ga/airticketmanagement/controller/AirportFlightController.java
@@ -1,13 +1,17 @@
 package com.ga.airticketmanagement.controller;
 
-import com.ga.airticketmanagement.dto.request.CreateFlightRequest;
+import com.ga.airticketmanagement.dto.request.FlightRequest;
 import com.ga.airticketmanagement.dto.request.UpdateFlightByOriginAirportRequest;
+import com.ga.airticketmanagement.dto.response.FlightResponse;
+import com.ga.airticketmanagement.dto.response.ListResponse;
 import com.ga.airticketmanagement.model.Flight;
-import com.ga.airticketmanagement.service.AirportService;
 import com.ga.airticketmanagement.service.FlightService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,44 +27,43 @@ public class AirportFlightController {
         this.flightService = flightService;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/{originAirportId}/flights")
-    public ResponseEntity<Flight> createFlightByOriginAirport(@PathVariable Long originAirportId, @RequestBody CreateFlightRequest flightObject) {
+    public FlightResponse createFlightByOriginAirport(@PathVariable Long originAirportId, @RequestBody FlightRequest flightObject) {
 
-        Flight flight = flightService.createFlightByOriginAirport(originAirportId, flightObject);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(flight);
+        return flightService.createFlightByOriginAirport(originAirportId, flightObject);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{airportId}/flights/{flightId}")
-    public ResponseEntity<Flight> updateFlight(@PathVariable Long airportId, @PathVariable Long flightId, @RequestBody UpdateFlightByOriginAirportRequest flightObject) {
+    public FlightResponse updateFlight(@PathVariable Long airportId, @PathVariable Long flightId, @RequestBody UpdateFlightByOriginAirportRequest flightObject) {
 
-        Flight flight = flightService.updateFlightByOriginAirport(airportId, flightId, flightObject);
-        return ResponseEntity.status(HttpStatus.OK).body(flight);
+        return flightService.updateFlightByOriginAirport(airportId, flightId, flightObject);
     }
 
     @GetMapping("/{airportId}/flights/{flightId}")
-    public Flight getFlight(@PathVariable Long airportId, @PathVariable Long flightId) {
+    public FlightResponse getFlight(@PathVariable Long airportId, @PathVariable Long flightId) {
 
-        return flightService.getFlight(airportId, flightId);
+        return flightService.getFlightOfAirport(airportId, flightId);
     }
 
     @GetMapping("/{airportId}/flights")
-    public List<Flight> getAirportFlights(@PathVariable Long airportId) {
+    public ListResponse<FlightResponse> getAirportFlights(@PathVariable Long airportId, Pageable pageable) {
 
-        return flightService.getAirportFlights(airportId);
+        return flightService.getAirportFlights(airportId, pageable);
     }
 
 
     @GetMapping("/{airportId}/departures")
-    public List<Flight> getAirportDepartures(@PathVariable Long airportId) {
+    public ListResponse<FlightResponse> getAirportDepartures(@PathVariable Long airportId, Pageable pageable) {
 
-        return flightService.getAirportDepartures(airportId);
+        return flightService.getAirportDepartures(airportId, pageable);
     }
 
     @GetMapping("/{airportId}/arrivals")
-    public List<Flight> getAirportArrivals(@PathVariable Long airportId) {
+    public ListResponse<FlightResponse> getAirportArrivals(@PathVariable Long airportId, Pageable pageable) {
 
-        return flightService.getAirportArrivals(airportId);
+        return flightService.getAirportArrivals(airportId, pageable);
     }
 
 
