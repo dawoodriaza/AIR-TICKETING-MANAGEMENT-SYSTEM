e9a6f650e22274f10a13c4fc2eb83e4bf020fde3
Husain Habib <habib.h.e@outlook.com>
2026-01-12 16:45:51 +0300

    implement token hashing

--- src/main/java/com/ga/airticketmanagement/util/TokenGenerator.java
+++ src/main/java/com/ga/airticketmanagement/util/TokenGenerator.java
@@ -1,5 +1,8 @@
 package com.ga.airticketmanagement.util;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Base64;
 
@@ -12,5 +15,15 @@ public class TokenGenerator {
         byte[] randomBytes = new byte[32];
         secureRandom.nextBytes(randomBytes);
         return encoder.encodeToString(randomBytes);
+    }
+
+    public static String hash(String token) {
+        try{
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = messageDigest.digest(token.getBytes(StandardCharsets.UTF_8));
+            return encoder.encodeToString(hash);
+        }catch(NoSuchAlgorithmException e){
+            throw new IllegalStateException(e);
+        }
     }
 }
