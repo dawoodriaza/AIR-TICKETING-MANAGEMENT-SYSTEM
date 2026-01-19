a06bb344f665911f3289468e170aaf8febcbb52e
Husain Habib <habib.h.e@outlook.com>
2026-01-20 01:16:31 +0300

    add constructors

--- src/main/java/com/ga/airticketmanagement/dto/request/AirportRequest.java
+++ src/main/java/com/ga/airticketmanagement/dto/request/AirportRequest.java
@@ -2,9 +2,13 @@ package com.ga.airticketmanagement.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class AirportRequest {
