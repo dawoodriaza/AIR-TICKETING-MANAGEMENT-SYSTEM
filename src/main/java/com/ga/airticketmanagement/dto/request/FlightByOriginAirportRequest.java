8cd506f70dbe8bc32ddc80e02fc26b840582c4db
Husain Habib <habib.h.e@outlook.com>
2026-01-20 01:49:49 +0300

    remove redundant DTOs, unify under FlightByOriginAirportRequest and FlightRequest

---
+++ src/main/java/com/ga/airticketmanagement/dto/request/FlightByOriginAirportRequest.java
@@ -0,0 +1,29 @@
+package com.ga.airticketmanagement.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class FlightByOriginAirportRequest {
+
+    @NotBlank
+    private Long destinationAirportId;
+
+    @NotBlank
+    private LocalDateTime departureTime;
+
+    @NotBlank
+    private LocalDateTime arrivalTime;
+
+    @NotBlank
+    private BigDecimal price;
+}
